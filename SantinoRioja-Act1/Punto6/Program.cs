db5b13c37c85bf45c8e333f0b79a7d11776cd22c
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-03-26 20:53:19 -0300

    Actualizo nombre de la carpeta

---
+++ SantinoRioja-Act1/Punto6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //6. Escribir un programa que lea el peso (en kilogramos) y la altura (en metros) de una persona
+            //, y mostrar por pantalla su índice de masa corporal (IMC) (El IMC se calcula dividiendo el peso entre el cuadrado de la altura).
+            float peso, altura, imc;
+
+            Console.WriteLine("Indique peso en Kilogramos(KG):");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indique altura en Metros(M):");
+            altura = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("El indice de masa corporal(IMC) es de:"+ imc);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SantinoRioja-Act1/punto2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Escribir un programa en el cual se ingresen cuatro números, calcular e informar la suma de los dos primeros y el producto del tercero y el cuarto.
+            int numero1, numero2, numero3, numero4;
+            string linea;
+            Console.Write("Ingrese el primer numero: ");
+            linea = Console.ReadLine();
+            numero1 = int.Parse(linea);
+
+            Console.Write("Ingrese el segundo numero: ");
+            linea = Console.ReadLine();
+            numero2 = int.Parse(linea);
+
+            Console.Write("Ingrese el tercer numero: ");
+            linea = Console.ReadLine();
+            numero3 = int.Parse(linea);
+
+            Console.Write("Ingrese el cuarto numero: ");
+            linea = Console.ReadLine();
+            numero4 = int.Parse(linea);
+
+            Console.Write("suma de 1 y 2:"+ (numero1+numero2));
+            Console.WriteLine();
+            Console.Write("producto de 3 y 4:"+ numero3*numero4);
+            Console.ReadKey();
+        }
+    }
+}
