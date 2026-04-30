26d85552afc635adfe5c2abf2ed0edbc14d3960f
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-04-30 12:45:36 -0300

    subo parte de la actividad 10

---
+++ SantinoRioja-Act10/Punto1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class matriz
+        //1- Crear una matriz de 2 filas y 5 columnas.
+        //Realizar la carga de componentes por columna(es decir primero ingresar toda la primer columna, luego la segunda columna y así sucesivamente)
+        //Imprimir luego la matriz.
+    {
+        private int [,] mat;
+        public void cargarMat()
+        {
+            mat= new int[2, 5];
+            for (int i = 0;i<5;i++)
+            {
+                for (int j = 0;j<2;j++)
+                {
+                    Console.WriteLine("Ingrese el numero en la columna:"+i+" en la fila:"+j);
+                    mat[j, i] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public void imprimir()
+        {
+        for(int i = 0; i<5;i++)
+            { for (int j = 0; j < 2; j++) { Console.WriteLine("Numero de matriz en columna:"+i+" fila:"+j+" " + mat[j,i]); } }
+        }
+        static void Main(string[] args)
+        {
+            matriz matriz1 = new matriz();
+            matriz1.cargarMat();
+            matriz1.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SantinoRioja-Act10/Punto2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class matriz
+    {//2- Crear una matriz de n * m filas (cargar n y m por teclado)
+     //Intercambiar la primer fila con la segunda.Imprimir luego la matriz.
+        private int[,] mat;
+        private int filas, columnas;
+        public void cargar()
+        {
+            Console.WriteLine("Ingrese la cantidad de filas");
+            filas= int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de columnas");
+            columnas=int.Parse(Console.ReadLine());
+            mat= new int[filas,columnas];
+            for (int i=0; i<filas; i++)
+            { for (int j = 0; j < columnas; j++) 
+                {
+                    Console.WriteLine("Ingrese el numero para la matriz en fila:"+i+" columna:"+j);
+                    mat[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public void intercambiar(){
+            int aux = mat[0, 0];
+            for (int i=0;i<columnas;i++)
+            {
+                 aux = mat[0, i];
+                mat[0, i] = mat[1,i];
+                mat[1, i] = aux;
+            }
+        }
+        public void imprimir()
+        {
+            for (int i=0;i<filas;i++)
+            {
+                for (int j=0;j<columnas;j++)
+                {
+                    Console.WriteLine(mat[i,j]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            matriz matriz1 = new matriz();
+            matriz1.cargar();
+            matriz1.intercambiar();
+            matriz1.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
