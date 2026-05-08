aeb575ce5db3662160ad8fd31a2a9459fef5e817
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-05-08 12:02:44 -0300

    Subo punto 1 y 2 de la actividad 12

---
+++ SantinoRioja-Act12/Punto1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class Empresa
+    {/*1. Sistema de Logística: Envíos por Sucursal
+Una empresa de correo tiene 3 sucursales principales.Cada sucursal procesa una
+cantidad diferente de paquetes por día dependiendo de su demanda.
+● Definir un vector de tipo string para los nombres de las 3 sucursales.
+● Definir una matriz irregular donde cada fila sea una sucursal y cada columna
+represente el peso (en kg) de cada paquete enviado.
+● Métodos:
+1. Cargar los nombres de las sucursales y, para cada una, preguntar cuántos
+paquetes se enviaron hoy para definir el tamaño de su fila.
+2. Cargar el peso de cada paquete.
+3. Imprimir el peso de todos los paquetes organizados por sucursal.
+4. Calcular e informar el peso total despachado por cada sucursal.
+5. Informar cuál es el paquete más pesado de toda la empresa y a qué sucursal
+pertenece.*/
+        private string[] sucursales;
+        private float[][] prodsKg;
+
+        public void crear() {
+            sucursales = new string[3];
+            prodsKg = new float[3][];
+            for (int i = 0; i < sucursales.Length; i++)
+            {
+                Console.WriteLine("Cuantos paquetes envio hoy la empresa nro:"+i+"?");
+                prodsKg[i] = new float[int.Parse(Console.ReadLine())];
+            }
+            
+        }
+        public void cargar()
+        {
+            for (int i = 0;i < sucursales.Length;i++)
+            {
+                Console.WriteLine("Ingrese el nombre de la sucursal nro:"+i);
+                sucursales[i] = Console.ReadLine();
+
+                for (int j = 0; j < prodsKg[i].Length; j++) {
+                    Console.WriteLine("Ingrese el peso del paquete nro:"+j+ " De la sucursal:"+ sucursales[i]+" Ingrsar en KG");
+                    prodsKg[i][j]= float.Parse(Console.ReadLine());
+                }
+            }
+
+        }
+        public void impPeso()
+        {
+            for (int i = 0; i < sucursales.Length; i++)
+            {
+                Console.WriteLine("Sucursal:" + sucursales[i]+ "   Peso productos: ");
+                for (int j = 0; j < prodsKg[i].Length; j++)
+                {
+                    Console.WriteLine(prodsKg[i][j]);
+                }
+            }
+        }
+        public void pesoTotal()
+        {
+            
+                for (int i = 0; i < sucursales.Length; i++)
+                {
+                    float total = 0;
+                    Console.WriteLine("Sucursal:" + sucursales[i] + "   Peso productos total: ");
+                    for (int j = 0; j < prodsKg[i].Length; j++)
+                    {
+                        total+= prodsKg[i][j];
+                    }
+                    Console.WriteLine(total+"KG");
+                }
+            
+        }
+        public void masPeso()
+        {
+            float pesado = 0;
+            int pesadoFila = 0;
+                int pesadoColumna = 0;
+                for (int i = 0; i < sucursales.Length; i++)
+                {
+                    
+                    for (int j = 0; j < prodsKg[i].Length; j++)
+                    {
+                       if (pesado< prodsKg[i][j])
+                    {
+                        pesado = prodsKg[i][j];
+                        pesadoFila = i;
+                        pesadoColumna = j;
+                    }
+                    }
+                }
+            Console.WriteLine("El producto mas pesado de la empresa es el Nro:"+pesadoColumna+ " De la sucursal "+ sucursales[pesadoFila] + " pesando "+ prodsKg[pesadoFila][pesadoColumna]+" KG");
+        }
+        static void Main(string[] args)
+        {
+            Empresa empresa1 = new Empresa();
+            empresa1.crear();
+            empresa1.cargar();
+            empresa1.impPeso();
+            empresa1.pesoTotal();
+            empresa1.masPeso();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SantinoRioja-Act12/Punto2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Cine
+       /* 2. Gestión de Complejo de Cine
+Un cine tiene 4 salas con diferentes capacidades de espectadores(la Sala 1 tiene 10
+asientos, la Sala 2 tiene 15, la Sala 3 tiene 8 y la Sala 4 tiene 12).
+● Definir una matriz irregular de 4 filas para representar los asientos.
+● Métodos:
+1. Inicializar la matriz con los tamaños de las salas mencionadas(sin
+intervención del operador).
+2. Crear un método de  Venta de Entradas  que permita cargar la edad del
+espectador en un asiento específico(fila y columna).
+3. Imprimir el mapa de ocupación de las salas indicando la edad del espectador
+en cada asiento.
+4. Calcular cuántos menores de edad (menos de 18 años) hay en cada sala.
+5. Informar cuál es el promedio de edad de los espectadores de todo el
+complejo.*/
+    {
+        private string[] salas;
+        private int[][] asientos;
+        public void cargar()
+        {
+            salas = new string[4];
+            asientos = new int[4][];
+            asientos[0] = new int[10];
+            asientos[1] = new int[15];
+            asientos[2] = new int[8];
+            asientos[3] = new int[12];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("Ingrese la edad del espectador en la sala:" + salas[i]+" Asiento:" + asientos[i][j]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
