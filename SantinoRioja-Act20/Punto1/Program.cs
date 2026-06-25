dcc65c3e8dfac58467529f6bb51c2e0db81eba05
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-06-25 10:12:23 -0300

    Empiezo actividad 20

---
+++ SantinoRioja-Act20/Punto1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    /*Actividad 1: Sistema de control de vuelos
+Problema:
+Una aerolínea administra los vuelos programados mediante un sistema orientado a objetos.
+Plantear una clase base Vuelo que contenga como propiedades: número de vuelo,
+destino y duración en horas. Luego definir una clase derivada VueloInternacional que
+herede de Vuelo e incluya una propiedad adicional llamada paisDestino.
+Crear una lista de objetos de tipo VueloInternacional, cargar 5 vuelos con sus datos
+desde consola y luego:
+ Mostrar los vuelos registrados y el país de destino.
+ Informar cuál es el vuelo con mayor duración.
+ Mostrar el orden de ejecución de los constructores.*/
+
+    class Vuelo
+    {
+        protected int numeroDeVuelo;
+        protected string destino;
+        protected float duracionHoras;
+        public Vuelo()
+        {
+            Console.WriteLine("--Clase Actual: Vuelo --");
+            Console.WriteLine("Ingrese la duracion en horas del vuelo");
+            duracionHoras = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero del vuelo");
+            numeroDeVuelo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el destino del vuelo");
+            destino = Console.ReadLine();
+        }
+
+        public float DuracionHoras {  get { return duracionHoras; } set { duracionHoras = value; } }
+        public int NumeroDeVuelo { get { return numeroDeVuelo; } set { numeroDeVuelo = value; } }
+        public string Destino { get { return destino; } set { destino = value; } }
+
+    }
+    class VueloInternacional : Vuelo
+    {
+        protected string paisDestino;
+        public VueloInternacional() {
+            Console.WriteLine("--Clase Actual: VueloInternacional --");
+            Console.WriteLine("Ingrese el pais de destino del vuelo");
+            paisDestino = Console.ReadLine();
+        }
+        public string PaisDestino { get { return paisDestino; } set { paisDestino = value; } }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            float masLargo =0;
+            List<VueloInternacional> vuelosInternacionales = new List<VueloInternacional>();
+            for (int i = 0;i<2;i++)
+            {
+               VueloInternacional vueloInt = new VueloInternacional();
+                if (vueloInt.NumeroDeVuelo > masLargo){
+                    masLargo = vueloInt.NumeroDeVuelo;
+                }
+                vuelosInternacionales.Add(vueloInt);
+            }
+            foreach (VueloInternacional m in vuelosInternacionales)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Numero del vuelo:"+m.NumeroDeVuelo);
+                Console.WriteLine("Destino del vuelo:" + m.Destino);
+                Console.WriteLine("Duracion del vuelo:" + m.DuracionHoras);
+                Console.WriteLine("Destino pais del vuelo:" + m.Destino);
+                Console.WriteLine("------------------------------------");
+                if (m.DuracionHoras==masLargo)
+                {
+                    Console.WriteLine("El vuelo mas largo es el numero "+ m.NumeroDeVuelo);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SantinoRioja-Act20/Punto2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    /*Actividad 2: Administración de entregas logísticas
+Problema:
+Una empresa de envíos desea registrar sus entregas mediante colaboración de
+clases.
+Crear una clase Paquete con propiedades: código, peso y destino.
+Crear una clase Despachador que contenga una lista de paquetes y un método para registrar nuevos
+paquetes.
+Desde el método Main, instanciar un Despachador, cargar 5 paquetes y mostrar:
+ Todos los paquetes registrados.
+ Cuántos superan los 10 kg de peso.
+ Cuántos tienen destino nacional (por ejemplo, “Argentina”).*/
+    class Paquete
+    {
+        protected int codigo;
+        protected float peso;
+        protected string destino;
+    }
+    class despachador
+    {
+        List<Paquete> paquetes = new List<Paquete>() ;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+Console.ReadKey();
+        }
+    }
+}
