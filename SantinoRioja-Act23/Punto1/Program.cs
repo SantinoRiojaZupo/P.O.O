76a6e8873e51b995798336814dcaede30917930d
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-08-06 10:03:59 -0300

    Inicio actvidad 23

---
+++ SantinoRioja-Act23/Punto1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    /*
+ 1-
+ En un servidor de red, las solicitudes de impresión se procesan dinámicamente en orden de
+llegada.
+ Crear la clase Documento que contenga como atributos privados:
+nombreArchivo (string) y cantidadPaginas (int). Definir sus
+propiedades de solo lectura y un constructor que reciba los parámetros nom
+y pag.
+ Crear la clase ServidorImpresion que administre una lista dinámica de
+documentos (List&lt;Documento&gt;).
+ Métodos en ServidorImpresion:
+1. AgregarDocumento(): Solicitar por teclado los datos de un
+documento y agregarlo al final de la lista utilizando .Add().
+2. ImprimirSiguiente(): Si la lista no está vacía, simular la
+impresión del primer documento de la lista (mostrar sus datos en
+consola) y removerlo de la colección mediante .RemoveAt(0). Si está
+vacía, advertir que no hay trabajos pendientes.
+3. MostrarColaPendiente(): Listar todos los documentos que
+están esperando ser impresos y la cantidad total de páginas
+acumuladas en la cola de espera utilizando .Count.*/
+    class Documento
+    {
+        private string nombreArchivo;
+        private int cantidadPaginas;
+
+        public Documento(string nom, int pag)
+        {
+            nombreArchivo = nom;
+            cantidadPaginas = pag;
+        }
+        public string NombreArchivo
+        {
+            get { return nombreArchivo; }
+            set { nombreArchivo = value; }
+        }
+        public int CantidadPaginas
+        {
+            get { return cantidadPaginas; }
+            set { cantidadPaginas = value; }
+        }
+    }
+        class ServidorImpresion
+    {
+        List<Documento> documentos = new List<Documento>();
+        public void AgregarDocumento()
+        {
+            Console.WriteLine("Ingrese el nombre del nuevo documento");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Ingrese la cantidad de paginas del nuevo documento");
+            int pag = int.Parse(Console.ReadLine());
+            documentos.Add(new Documento(nom,pag));
+        }
+        public void ImprimirSiguiente()
+        {
+            if (documentos.Count > 0)
+            {
+                    Console.WriteLine("Nombre Documento:"+documentos[0].NombreArchivo+" paginas:" + documentos[0].CantidadPaginas);
+                documentos.RemoveAt(0);
+                    if (documentos.Count == 0)
+                    {
+                        Console.WriteLine("No hay trabajos pendientes");
+                    }
+            }
+        }
+        public void MosrarColaPendiente()
+        {
+            if (documentos.Count == 0)
+            {
+                Console.WriteLine("No hay documentos que listar");
+            }
+            else
+            {
+                int totalDocumentos, totalPaginas;
+                totalPaginas = 0;
+                totalDocumentos = documentos.Count;
+                Console.WriteLine("Listado de Documentos:");
+                foreach (Documento documento in documentos)
+                {
+                    Console.WriteLine(documento.NombreArchivo);
+                    totalPaginas += documento.CantidadPaginas;
+                }
+                Console.WriteLine("Documentos totales:" + totalDocumentos);
+                Console.WriteLine("Paginas totales acumuladas:" + totalPaginas);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServidorImpresion servidor1 = new ServidorImpresion();
+            servidor1.AgregarDocumento();
+            servidor1.AgregarDocumento();
+            servidor1.AgregarDocumento();
+            servidor1.ImprimirSiguiente();
+            servidor1.MosrarColaPendiente();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SantinoRioja-Act23/Punto2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{ /*2-
+Una empresa de logística autónoma monitorea el estado y nivel de batería de sus
+drones de entrega en vuelo de regreso a la base.
+ Crear la clase Dron que contenga los atributos privados: codigo (string) y
+nivelBateria (int, de 0 a 100). Definir sus propiedades correspondientes.
+Su constructor debe recibir cod y bat.
+ Crear la clase CentroControl que administre una lista de objetos
+List&lt;Dron&gt;.
+ Métodos en CentroControl:
+
+1. Un constructor que cargue por teclado una lista inicial de 4 drones
+ingresando sus códigos y baterías.
+2. ListarFlota(): Mostrar la lista de drones en pantalla.
+3. RemoverDronesBajos(): Recorrer la lista y remover por
+completo de la flota a todos aquellos drones cuyo nivel de batería
+sea menor o igual al 15% (ya que requieren mantenimiento
+automático urgente).
+4. MostrarDronesRestantes(): Imprimir la flota actualizada y la
+cantidad de drones operativos utilizando la propiedad .Count.*/
+    class Dron {
+        private string codigo;
+        private int nivelBateria;
+
+        public Dron(string cod, int bat)
+        {
+            codigo = cod;
+            nivelBateria = bat;
+        }
+        public string Codigo
+        {
+            get {  return codigo; }
+            set { codigo = value; }
+            
+        }
+        public int NivelBateria
+        {
+            get { return nivelBateria; }
+            set { nivelBateria = value; }
+        }
+
+    }
+    class CentroControl
+    {
+        List<Dron> drones = new List<Dron>();
+        public CentroControl()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                string cod;
+                int bat;
+                Console.WriteLine("Ingrese codigo del dron");
+                cod = Console.ReadLine();
+                Console.WriteLine("Ingrese ");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
